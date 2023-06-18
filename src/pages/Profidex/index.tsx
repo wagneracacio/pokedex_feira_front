730d5581f6660cffbf261d4272bb6fa7995b7b93
Wagner <wmaluk@gmail.com>
2023-06-18 01:20:38 -0300

    adicionado hash de checagem de tipo

--- src/pages/Profidex/index.tsx
+++ src/pages/Profidex/index.tsx
@@ -1,24 +1,19 @@
 import { Button } from "react-bootstrap";
-import { ProfilePicture } from "../../components/ProfilePicture";
 import { BsQrCodeScan } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import PageLayout from "../Layout";
 import QRCode from "react-qr-code";
 import { useAppDispatch, useAppSelector } from "../../redux/hook";
-import { addFriend } from "../../redux/features/auth/thunks";
-import { AnyAction } from "@reduxjs/toolkit";
-import { useRef } from "react";
-import { getAllUsers } from "../../redux/features/user/thunks";
+import { userHashCheck } from "../../config/credentials";
 //BsQrCodeScan
 
 export const Profidex = () => {
   const navigate = useNavigate();
-  const dispatch = useAppDispatch();
   const { user } = useAppSelector((state) => state.Auth);
   return (
     <PageLayout title="Scan">
       <div className="d-flex justify-content-center">
-        <QRCode value={user!.uid || "hey"} />
+        <QRCode value={`${userHashCheck}${user!.uid}`} />
       </div>
       <h5 className="d-flex justify-content-center">Seu QrCode</h5>
       <div className="d-flex justify-content-center mt-5">
